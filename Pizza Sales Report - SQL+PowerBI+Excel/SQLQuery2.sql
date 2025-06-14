bd167714359d291d02bdf1bb7c760fd0625ffd8a
manish121096 <85885396+manish121096@users.noreply.github.com>
2025-06-14 19:14:07 +0530

    Add files via upload

---
+++ Pizza Sales Report - SQL+PowerBI+Excel/SQLQuery2.sql
@@ -0,0 +1,27 @@
+select * from pizza_sales;
+
+-- Aggrigation funciton (KPI's)
+
+SELECT SUM(total_price) AS Total_Revenue from pizza_sales
+
+SELECT SUM(total_price)/COUNT(DISTINCT order_id) AS Total_AVG_Value FROM pizza_sales
+
+SELECT SUM(quantity) AS Total_pizza_sold from pizza_sales;
+
+SELECT COUNT(DISTINCT order_id) as Total_No_of_order from pizza_sales
+
+SELECT CAST(CAST(SUM(quantity) AS DECIMAL(10,2))/
+		CAST(COUNT(DISTINCT order_id) AS DECIMAL(10,2)) AS DECIMAL(10,2)) AS Average_pizza_per_order  from pizza_sales
+
+--Charts and Requrements
+
+SELECT DATENAME(DW, order_date) as Order_Day, COUNT(DISTINCT order_id) from pizza_sales
+	GROUP BY DATENAME(DW, order_date)
+	
+SELECT DATENAME(MONTH, order_date) as Order_Month, COUNT(DISTINCT order_id) as Order_Count from pizza_sales
+	GROUP BY DATENAME(MONTH, order_date)
+
+SELECT pizza_category, CAST(SUM(total_price)*100/(SELECT SUM(total_price) from pizza_sales) AS DECIMAL(10,2)) as Total_Revenue from pizza_sales
+	GROUP BY pizza_category
+	ORDER BY Total_Revenue DESC
+
